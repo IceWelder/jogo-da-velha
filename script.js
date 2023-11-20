4951ad1475cb8c664dd45cd13e88f83032d4c513
Pedro Augusto <102700254+IceWelder@users.noreply.github.com>
2023-11-20 18:39:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,23 @@
-let jogador = "1";
+let jogador = "x";
 
-function play(cell){
-
-    if(cell.innerHTML === ""){
-        cell.innerHTML = jogador;
-    } else{
-        return
-    }
-
-
-    if(jogador == "1"){
-       jogador = "0"; 
-
-    } else{
-        jogador = "1"
-    }
+// Function to render the current state of the game
+function render() {
+    // Implement your rendering logic here.
+    // For example, you might update the UI to reflect the current state of the game.
 }
 
+function play(cell) {
+    if (cell.innerHTML === "") {
+        cell.innerHTML = jogador;
+        render(); // Call the render function after a move is made.
+    } else {
+        return;
+    }
 
-
-
-
-
-
-
-
-
-
-
+    // Switch players for the next turn.
+    if (jogador == "x") {
+        jogador = "o";
+    } else {
+        jogador = "x";
+    }
+}
